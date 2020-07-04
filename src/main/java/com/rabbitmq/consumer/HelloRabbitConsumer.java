74db4d552d4bd7ab484f8d9743cb08a6d3734ebf
M Firman Akbar <em.firman.akbar@gmail.com>
2020-07-04 14:46:21 +0700

    adding RabbitMQ consumer sample

---
+++ src/main/java/com/rabbitmq/consumer/HelloRabbitConsumer.java
@@ -0,0 +1,14 @@
+package com.rabbitmq.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloRabbitConsumer {
+
+    @RabbitListener(queues = "testproducer.hello")
+    public void listen(String message) {
+        System.out.println("consuming " + message);
+    }
+
+}
